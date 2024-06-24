f420ab7a3584f42283c1b00e0de5eca2ed78c600
Galen <galen2@hotmail.com>
2024-06-24 15:47:00 +0200

    Frontend: Componente Navbar Responsive. Arreglado color de botón rechazar cookies en Componente Cookie. WIP: Componente Footer responsive.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useIntl } from "react-intl";
 import styles from "../styles/Navbar.module.css";
@@ -12,15 +12,31 @@ const Navbar: React.FC<NavbarProps> = ({ onLocaleChange, currentLocale }) => {
   const intl = useIntl();
   const [showRestaurants, setShowRestaurants] = useState(false); // Estado para controlar la visibilidad del menú desplegable
   const [mobileMenu, setMobileMenu] = useState(false); // Estado para controlar la apertura del menú
+  const [isMobile, setIsMobile] = useState(false); // Estado para detectar el modo móvil
 
-  const toggleMobileMenu = () => setMobileMenu(!mobileMenu); // Función para alternar el estado del menú de movil
+  useEffect(() => {
+    const handleDevice = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    handleDevice(); // Verificar al cargar la página
+    window.addEventListener("resize", handleDevice);
+    return () => window.removeEventListener("resize", handleDevice);
+  }, []);
+
+  const toggleMobileMenu = () => setMobileMenu(!mobileMenu); // Función para alternar el estado del menú de móvil
+  const closeMobileMenu = () => {
+    if (mobileMenu) {
+      setMobileMenu(false);
+    }
+  };
 
   return (
     <div className={styles.navbar}>
       <div className={styles.navbarTop}>
         <div className={styles.imgLogoContainer}>
-          <Link href="/">
-            <img src="/images/navbar/imagenLogo.png" alt="Logo Paraiso Del Jamón" className={styles.imgLogo} />
+          <Link href="/" onClick={closeMobileMenu}>
+            <img src="/images/navbar/imagenLogo.png" alt="Logo Paraíso Del Jamón" className={styles.imgLogo} />
           </Link>
         </div>
         <div className={styles.textLogoContainer}>
@@ -28,34 +44,103 @@ const Navbar: React.FC<NavbarProps> = ({ onLocaleChange, currentLocale }) => {
         </div>
         <div className={styles.flagContainer}>
           <div className={styles.mobileMenuIcon} onClick={toggleMobileMenu}>
-            {mobileMenu ? "X" : "☰"} {/* Cambia entre X y el icono de menú */}
+            <img
+              src={mobileMenu ? "/images/navbar/cerrar.png" : "/images/navbar/menu.png"}
+              alt={mobileMenu ? "Cerrar menú" : "Abrir menú"}
+              className={styles.menuIcon}
+            />
           </div>
           <div className={styles.flags}>
-            <img src="/images/flags/es.png" alt="Español" className={styles.flag} onClick={() => onLocaleChange("es")} />
-            <img src="/images/flags/en.png" alt="English" className={styles.flag} onClick={() => onLocaleChange("en")} />
-            <img src="/images/flags/de.png" alt="Deutsch" className={styles.flag} onClick={() => onLocaleChange("de")} />
+            <img
+              src="/images/flags/es.png"
+              alt="Español"
+              className={styles.flag}
+              onClick={() => {
+                onLocaleChange("es");
+                closeMobileMenu();
+              }}
+            />
+            <img
+              src="/images/flags/en.png"
+              alt="English"
+              className={styles.flag}
+              onClick={() => {
+                onLocaleChange("en");
+                closeMobileMenu();
+              }}
+            />
+            <img
+              src="/images/flags/de.png"
+              alt="Deutsch"
+              className={styles.flag}
+              onClick={() => {
+                onLocaleChange("de");
+                closeMobileMenu();
+              }}
+            />
           </div>
         </div>
       </div>
-      <div className={styles.navbarMenu}>
+      <div className={`${styles.navbarMenu} ${mobileMenu ? styles.showMenu : ""}`}>
         <div className={styles.links}>
-          <Link href="/">{intl.formatMessage({ id: "Navbar_inicio" })}</Link>
-          <div className={styles.linksDropdown} onMouseEnter={() => setShowRestaurants(true)} onMouseLeave={() => setShowRestaurants(false)}>
-            <span className={styles.noLink}>{intl.formatMessage({ id: "Navbar_restaurantes" })}</span>
-            <div className={`${styles.dropdown} ${showRestaurants ? styles.show : ""}`}>
-              <Link href="/san-bernardo">San Bernardo</Link>
-              <Link href="/bravo-murillo">Bravo Murillo</Link>
-              <Link href="/reina-victoria">Reina Victoria</Link>
-              <Link href="/arenal">Arenal</Link>
+          <Link href="/" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_inicio" })}
+          </Link>
+          {!isMobile ? (
+            <div className={styles.linksDropdown} onMouseEnter={() => setShowRestaurants(true)} onMouseLeave={() => setShowRestaurants(false)}>
+              <span className={styles.noLink}>{intl.formatMessage({ id: "Navbar_restaurantes" })}</span>
+              <div className={`${styles.dropdown} ${showRestaurants ? styles.show : ""}`}>
+                <Link href="/san-bernardo" onClick={closeMobileMenu}>
+                  San Bernardo
+                </Link>
+                <Link href="/bravo-murillo" onClick={closeMobileMenu}>
+                  Bravo Murillo
+                </Link>
+                <Link href="/reina-victoria" onClick={closeMobileMenu}>
+                  Reina Victoria
+                </Link>
+                <Link href="/arenal" onClick={closeMobileMenu}>
+                  Arenal
+                </Link>
+              </div>
             </div>
-          </div>
-          <Link href="/reservas">{intl.formatMessage({ id: "Navbar_reservas" })}</Link>
-          <Link href="/menu">{intl.formatMessage({ id: "Navbar_menu" })}</Link>
-          <Link href="/carta">{intl.formatMessage({ id: "Navbar_carta" })}</Link>
-          <Link href="/charcuteria">{intl.formatMessage({ id: "Navbar_charcuteria" })}</Link>
-          <Link href="/about">{intl.formatMessage({ id: "Navbar_about" })}</Link>
-          <Link href="/blog">{intl.formatMessage({ id: "Navbar_blog" })}</Link>
-          <Link href="/contacto">{intl.formatMessage({ id: "Navbar_contacto" })}</Link>
+          ) : (
+            <div className={`${styles.dropdown} ${styles.show}`}>
+              <Link href="/san-bernardo" onClick={closeMobileMenu}>
+                San Bernardo
+              </Link>
+              <Link href="/bravo-murillo" onClick={closeMobileMenu}>
+                Bravo Murillo
+              </Link>
+              <Link href="/reina-victoria" onClick={closeMobileMenu}>
+                Reina Victoria
+              </Link>
+              <Link href="/arenal" onClick={closeMobileMenu}>
+                Arenal
+              </Link>
+            </div>
+          )}
+          <Link href="/reservas" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_reservas" })}
+          </Link>
+          <Link href="/menu" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_menu" })}
+          </Link>
+          <Link href="/carta" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_carta" })}
+          </Link>
+          <Link href="/charcuteria" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_charcuteria" })}
+          </Link>
+          <Link href="/about" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_about" })}
+          </Link>
+          <Link href="/blog" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_blog" })}
+          </Link>
+          <Link href="/contacto" onClick={closeMobileMenu}>
+            {intl.formatMessage({ id: "Navbar_contacto" })}
+          </Link>
         </div>
       </div>
     </div>
