16a309763ff52010417a6d2c80b931c49fe8270f
Galen <galen2@hotmail.com>
2024-10-24 17:13:10 +0200

    Frontend: Actualizadas librerias. Actualizada version sitio web. Eliminado efecto hover en Navbar de cambio de fondo de elementos. Añadidos textos de San Bernardo internacionalizados.

--- frontend/src/pages/san-bernardo.tsx
+++ frontend/src/pages/san-bernardo.tsx
@@ -3,6 +3,7 @@ import { useIntl } from "react-intl";
 import { useVisitedPageTracking } from "../hooks/useVisitedPageTracking";
 import { useVisitedPageTrackingGA } from "../hooks/useTrackingGA";
 import Loader from "../components/Loader";
+import useScrollToTop from "../hooks/useScrollToTop";
 import AnimatedTitle from "../components/AnimatedTitle";
 import Map from "../components/Map";
 import styles from "../styles/san-bernardo.module.css";
@@ -17,6 +18,8 @@ const SanBernardo = ({ loadingMessages, mapLocale, cookiesModalClosed }: SanBern
   let restaurante = "san-bernardo";
   const intl = useIntl();
 
+  const { isScrollButtonVisible, scrollToTop } = useScrollToTop();
+
   useVisitedPageTracking(restaurante);
   useVisitedPageTrackingGA(restaurante);
 
@@ -29,8 +32,36 @@ const SanBernardo = ({ loadingMessages, mapLocale, cookiesModalClosed }: SanBern
   return (
     <div className="pageContainer">
       <AnimatedTitle text1Id="sanBernardo_Titulo_Texto1" text2Id="sanBernardo_Titulo_Texto2" cookiesModalClosed={cookiesModalClosed} />
-      <p>{intl.formatMessage({ id: "sanBernardo_Descripcion" })}</p>
+      <p>{intl.formatMessage({ id: "sanBernardo_Texto1" })}</p>
+      <p>{intl.formatMessage({ id: "sanBernardo_Texto2" })}</p>
+      <p>{intl.formatMessage({ id: "sanBernardo_Texto3" })}</p>
+      <br></br>
+      <div className={styles.localesContainer}>
+        <div className={styles.contactLocation}>
+          <h3 className="text-center">{intl.formatMessage({ id: "contacto_Informacion_SanBernardo_Titulo" })}</h3>
+          <p>
+            <span className="fw-bold">{intl.formatMessage({ id: "contacto_Informacion_SanBernardo_Direccion_Texto" })}</span>
+            {intl.formatMessage({ id: "contacto_Informacion_SanBernardo_Direccion_Calle" })}
+          </p>
+          <p>
+            <span className="fw-bold">{intl.formatMessage({ id: "contacto_Informacion_SanBernardo_Telefono_Texto" })}</span>
+            <a className={styles.link} href="tel:+345328350">
+              {intl.formatMessage({ id: "contacto_Informacion_SanBernardo_Telefono_Numero" })}
+            </a>
+          </p>
+          <p>
+            <span className="fw-bold">{intl.formatMessage({ id: "contacto_Informacion_SanBernardo_Horario_Texto" })}</span>
+            {intl.formatMessage({ id: "contacto_Informacion_SanBernardo_Horario_Numero" })}
+          </p>
+        </div>
+      </div>
+      <br></br>
       <Map locationKey={locationKey} mapLocale={mapLocale} />
+      {isScrollButtonVisible && (
+        <button onClick={scrollToTop} className="scrollTop">
+          <img src="/images/web/flechaArriba.png" alt="Subir" />
+        </button>
+      )}
     </div>
   );
 };
