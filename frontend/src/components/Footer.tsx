09523ee18e2a1a153383f7401281e8ac50834fc4
Galen <galen2@hotmail.com>
2024-11-07 15:13:31 +0100

    Frontend: Arreglado desbordamiento horizontal en modo movil. Cambiada lógica para formato de componente Footer de JS a CSS.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { useIntl } from "react-intl";
 import Link from "next/link";
-import useDeviceType from "../hooks/useDeviceType";
 import { useMenu } from "../contexts/MenuContext";
 import Loader from "../components/Loader";
 import styles from "../styles/components/Footer.module.css";
@@ -20,27 +19,21 @@ export interface FooterProps {
  * Componente Footer
  *
  * Muestra el pie de página con enlaces a páginas de políticas y un mensaje de derechos reservados.
- * Detecta si el dispositivo es móvil para ajustar el diseño y cierra el menú móvil cuando se hace clic en los enlaces.
+ * Cierra el menú móvil cuando se hace clic en los enlaces.
  *
  * @param {FooterProps} props - Propiedades del componente Footer.
  * @returns {JSX.Element} Pie de página con enlaces y loader opcional.
  */
 const Footer: React.FC<FooterProps> = ({ loadingMessages }: FooterProps): JSX.Element => {
   const intl = useIntl(); // Hook para obtener mensajes localizados
-
-  const deviceType = useDeviceType(); // Detecta el tipo de dispositivo
   const { closeMobileMenu } = useMenu(); // Cierra el menú móvil si está abierto
-
-  const isMobile = deviceType === "mobile"; // Determina si el dispositivo es móvil
 
   /**
    * Función para manejar el clic en los enlaces.
-   * Si el dispositivo es móvil, cierra el menú al hacer clic en cualquier enlace.
+   * Cierra el menú al hacer clic en cualquier enlace.
    */
   const handleLinkClick = () => {
-    if (isMobile) {
-      closeMobileMenu();
-    }
+    closeMobileMenu();
   };
 
   // Enlaces de las políticas legales
@@ -68,14 +61,10 @@ const Footer: React.FC<FooterProps> = ({ loadingMessages }: FooterProps): JSX.El
     <footer className={styles.footer}>
       <div>
         <p>
-          {/* Muestra los derechos reservados y el año actual */}
-          {intl.formatMessage({ id: "Footer_Rights" }, { year: new Date().getFullYear() })}
-          {!isMobile && " | "}
-          {/* Muestra los enlaces solo si no es móvil */}
-          {!isMobile && links}
+          <span className={styles.rightsText}>{intl.formatMessage({ id: "Footer_Rights" }, { year: new Date().getFullYear() })}</span>
+          <span className={styles.separator}> | </span>
+          <span className={styles.links}>{links}</span>
         </p>
-        {/* Muestra los enlaces en una nueva línea si es móvil */}
-        {isMobile && <p>{links}</p>}
       </div>
     </footer>
   );
